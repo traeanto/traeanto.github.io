5e9469ef7442013c8f9e450e50db9bce7010645e
Nilvaela kun <traeanto@gmail.com>
2023-04-29 18:26:17 +0700

    UPDATE: Parallax scrolling effect fix

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,19 +1,23 @@
 document.addEventListener('mousemove', parallax);
 function parallax(e) {
-	this.querySelectorAll('.particle-layer').forEach(function(layer) {
-		const speed = layer.getAttribute('data-speed');
+	const layer = document.querySelector('.header-particle-wrapper');
+	if(window.scrollY < layer.offsetHeight) {
+		this.querySelectorAll('.particle-layer').forEach(function(layer) {
+			const speed = layer.getAttribute('data-speed');
 
-		const x = (window.innerWidth + e.pageX*speed)/100;
-		const y = (window.innerHeight + e.pageY*speed)/100;
+			const x = (window.innerWidth + e.pageX*speed)/100;
+			const y = (window.innerHeight + e.pageY*speed)/100;
 
-		layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
-	});
+			layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+		});
+	}
 }
 
 window.addEventListener('scroll', function() {
-	let scroll = (window.scrollY/1000)+1;
-	console.log(scroll);
-
-	const layer = document.getElementById('header-particle-wrapper');
-	layer.style.transform = `scale(${scroll})`;
+	const layer = document.querySelector('.header-particle-wrapper');
+	if(window.scrollY < layer.offsetHeight) {
+		let scroll = (window.scrollY / 1000) + 1;
+		
+		layer.style.transform = `scale(${scroll})`;
+	}
 });

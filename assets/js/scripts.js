b72360b0fd8fecab05c0a78744ef516aa8d62925
Nilvaela kun <traeanto@gmail.com>
2023-04-29 16:55:19 +0700

    UPDATE: Parallax scrolling effect

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,19 @@
-document.addEventListener("mousemove", parallax);
+document.addEventListener('mousemove', parallax);
 function parallax(e) {
-	this.querySelectorAll('.particle-layer').forEach(layer => {
-		const speed = layer.getAttribute('data-speed')
+	this.querySelectorAll('.particle-layer').forEach(function(layer) {
+		const speed = layer.getAttribute('data-speed');
 
-		const x = (window.innerWidth + e.pageX*speed)/100
-		const y = (window.innerHeight + e.pageY*speed)/100
+		const x = (window.innerWidth + e.pageX*speed)/100;
+		const y = (window.innerHeight + e.pageY*speed)/100;
 
-		layer.style.transform = `translateX(${x}px) translateY(${y}px)`
-	})
+		layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+	});
 }
+
+window.addEventListener('scroll', function() {
+	let scroll = (window.scrollY/1000)+1;
+	console.log(scroll);
+
+	const layer = document.getElementById('header-particle-wrapper');
+	layer.style.transform = `scale(${scroll})`;
+});
